5842504b2e551c3721739fc72cc7c78e212abf24
63070162 <73683141+63070162@users.noreply.github.com>
2020-10-30 12:34:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<?php
-echo "<h2>IT@KMITL</h2>;
-echo "Hello IT<br>";
-?>
+<html>
+    <head>
+        <title>Wutiporn jamkrajang Art</title>
+    </head>
+    <body>
+        <h1></h1>
+        <p>A man was waiting for an interview with foreigner boss.
+           When the boss arrived,He said "Are you ready?"
+           Thai man replied  "No,I'm a man not lady."</p>
+        <img src="https://img.kyodonews.net/english/public/images/posts/999dc7ea8c73f32baa5361320e39c19a/photo_l.jpg">
+        <img src="https://i.ytimg.com/vi/1Ne1hqOXKKI/maxresdefault.jpg>\">
+        <br>
+        <a href="https://www.facebook.com/">Facebook<a/>
+        <a href="https://www.youtube.com/">Youtube Link</a>
+
+    </body>
+</html>
